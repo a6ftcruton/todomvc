11061a9ac3372ac754d992f1d7d1f01550f0372f
Jim Sutton <jimsuttonjimsutton@gmail.com>
2015-02-24 11:03:19 -0700

    Add localStorage to store todo.

--- jquery/scripts.js
+++ jquery/scripts.js
@@ -7,14 +7,29 @@ var App = {
     this.createTodo();   
   },
 
-  renderTodos: function() {
-    
-  },
-
   createTodo: function() {
-    $('#new-todo').focus(function(){
-      console.log("clicking in input");
+    $('#new-todo').keydown(function(e){
+      var name = $('#new-todo').val();
+      if(e.keyCode == 13) {
+        if(!name.trim()) {
+          $('#alert').html("<p>Cannot submit a blank todo</p>")
+                     .fadeIn("slow").delay(1400).fadeOut("slow");
+          return false; //prevent browser from reading rest of this script
+        } else {
+            $('#todo-list').prepend('<li class="todo">' + name + '</li>' + '<h2 class="delete-x">' + "X" + '</h2>')
+            $('#form')[0].reset();
+            var todos = $('#todo-list').html();
+            localStorage.setItem("todos", todos);
+            return false;
+        }
+      }
     });
+  }, //end createTodo
+
+  renderTodos: function() {
+    if(localStorage.getItem('todos')) {
+      $('#todo-list').html(localStorage.getItem('todos')); 
+    }
   }
 
 }; // end App
