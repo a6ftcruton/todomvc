6acf6ce46ee78ee6f75ee1fe2a5b4f2881671903
Jim Sutton <jimsuttonjimsutton@gmail.com>
2015-02-25 16:28:59 -0700

    Move script tags below html ya big dummy

--- jquery/scripts.js
+++ jquery/scripts.js
@@ -1,5 +1,11 @@
 'use strict'
 
+// Save common selectors
+var jQ = {
+  newTodo: $("#new-todo"), 
+  todoList: $("#todo-list")
+}
+
 var App = {
 
   init: function() {
@@ -15,21 +21,23 @@ var App = {
   },
 
   createTodo: function() {
-    $('#new-todo').keydown(function(e){
-      var name = $('#new-todo').val();
+
+    jQ.newTodo.keydown(function(e){
+
+      var input = jQ.newTodo.val();
+
       if(e.keyCode == 13) {
-        if(!name.trim()) {
+        if(!input.trim()) {
           $('#alert').html("<p>Cannot submit a blank todo</p>")
                      .fadeIn("slow").delay(1400).fadeOut("slow");
           return false; //prevent browser from reading rest of this script
         } else {
-          console.log($(this));
+            var todos;
             var clear = '<h3 class="delete">' + "X" + '</h3>';
-            var todo = { name: "<li class='todo name'>" + name + clear + "</li>", }
-            $('#todo-list').append(todo.name);
-            //$('#todo-list').prepend('<li class="todo name">' + name + '<h3 class="delete">' + "X" + '</h3>' + '</li>' )
+            var todo = { name: "<li class='todo name'>" + input + clear + "</li>", }
+            jQ.todoList.append(todo.name);
             $('#form')[0].reset();
-            var todos = $('#todo-list').html();
+            todos = jQ.todoList.html();
             localStorage.setItem("todos", todos);
             return false;
         }
@@ -39,13 +47,11 @@ var App = {
 
   renderTodos: function() {
     if(localStorage.getItem('todos')) {
-      $('#todo-list').html(localStorage.getItem('todos')); 
+      jQ.todoList.html(localStorage.getItem('todos')); 
     }
   }
 
 }; // end App
 
-$(document).ready(function() {
-  App.init();
-});
+App.init();
 
